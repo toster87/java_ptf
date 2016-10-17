6c36db74347c0ceb92577f411dd1b82bb990306c
toster87 <toster87@mail.ru>
2016-10-17 15:59:32 +0300

    Клик по элементу

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ContactHelper.java
@@ -7,34 +7,24 @@ import ru.stqa.ptf.addressbook.model.ContactData;
 /**
  * Created by legai on 17.10.2016.
  */
-public class ContactHelper {
-  private ChromeDriver wd;
+public class ContactHelper extends HelperBase{
 
   public ContactHelper(ChromeDriver wd) {
-    this.wd = wd;
+    super(wd);
 
   }
 
-  public void returnToHomePage() {
-      wd.findElement(By.linkText("home page")).click();
+  public void returnToHomePage() {wd.findElement(By.linkText("home page")).click();
   }
 
-  public void submitContactCreation() {
-      wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  public void submitContactCreation() {wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
   }
 
   public void fillContactForm(ContactData contactData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-      wd.findElement(By.name("mobile")).click();
-      wd.findElement(By.name("mobile")).clear();
-      wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    type(By.name("firstname"), contactData.getFirstname());
+    type(By.name("lastname"), contactData.getLastname());
+    type(By.name("mobile"), contactData.getMobile());
+    type(By.name("email"), contactData.getEmail());
   }
+
 }
